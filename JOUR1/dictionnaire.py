5d3c735593a4b6fcfa89e822b1759d14a650fa2e
Toufik FETTOUS <fettous.toufik@outlook.com>
2020-12-07 16:15:56 +0100

    jour1

---
+++ JOUR1/dictionnaire.py
@@ -0,0 +1,8 @@
+#coding:utf8
+dictionnaire = {}
+dictionnaire['plane']= 'avion'
+dictionnaire['car']= 'voiture'
+dictionnaire['cat']= 'chat'
+dictionnaire['house']= 'maison'
+dictionnaire['job']= 'travail'
+print (dictionnaire)
